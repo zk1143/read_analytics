ebdf298cf393d07a09c0fe8f31de08a49d03e909
zk1143 <134837799+zk1143@users.noreply.github.com>
2023-06-03 00:21:23 +0800

    Add files via upload

---
+++ read.py
@@ -0,0 +1,17 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+print('總共', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('平均留言字數為' ,sum_len/len(data),)	
+
+
